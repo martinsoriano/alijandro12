0d7dfb0bffff586a178fe08b5aa62460f0e1662e
martinsoriano <58281936+martinsoriano@users.noreply.github.com>
2020-02-14 09:49:58 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Homepage</title>
+  <title>index</title>
 </head>
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
-
-	<table width='80' border=0>
-
-	<tr bgcolor="=#CCCCCC">
-	    <td>Name</td>
-	    <td>Age</td>
-	    <td>Email</td>
-	    <td>update</td>
-	   </tr>
-<?php
-   while($res = mysqli_fetch_array($result)){
-   echo "<tr>";
-   echo "<td>".$res["name"]."</td>";
-   echo "<td>".$res["age"]."</td>";
-   echo "<td>".$res["email"]."</td>";
-   echo "<td><a href="\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delte.php?id=$res[id]\" onClick=\"return
-        confirm("Are you sure you want to delete?.)\">
-        Delete</td></td>";
-     
-     }
-
-
-?>
-</table>
 
 </body>
 </html>
